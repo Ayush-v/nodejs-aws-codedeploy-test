4a1c256ba1d531751e0372c4943e81d6b6cee412
Ayush <62958067+Ayush-v@users.noreply.github.com>
2022-11-19 10:08:24 +0530

    changed scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
 
-echo 'cd /home/ec2-user/nodejs-aws-codedeploy-test' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+echo 'cd /home/ec2-user/nodejs-aws-codedeploy-test' >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
 cd /home/ec2-user/nodejs-aws-codedeploy-test >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
 
 echo 'npm install' >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log 
