bfeef2e6c6b273be246aa11af972c3645951a2fd
Ayush <62958067+Ayush-v@users.noreply.github.com>
2022-11-19 12:47:43 +0530

    working

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#give permission for everything in the express-app directory
+sudo chmod -R 777 /home/ec2-user/express-app
+
 echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
 
 echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
