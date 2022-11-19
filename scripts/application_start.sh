84a940bd66cc14b29dc558fa24105487f4b1e3a4
Ayush <62958067+Ayush-v@users.noreply.github.com>
2022-11-19 14:00:19 +0530

    testing

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,7 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
 
+
+cd /home/ec2-user/nodejs-aws-codedeploy-test
 echo 'sudo pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
 pm2 restart nodejs-express-app >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
