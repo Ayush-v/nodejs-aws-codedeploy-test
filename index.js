9724cad391aadd318bb11ecbc0edd8b684b98423
Ayush <62958067+Ayush-v@users.noreply.github.com>
2022-11-19 15:06:30 +0530

    working test 2

--- index.js
+++ index.js
@@ -40,29 +40,6 @@ app.get("/getdata", function (req, res) {
         bs: "synergize scalable supply-chains",
       },
     },
-    {
-      id: 1232343,
-      name: "It Might Work",
-      username: "last 2 steps",
-      email: "Shsdfasrgadrfgearanna@melissa.tv",
-      address: {
-        street: "Victor Plains",
-        suite: "Suite 879",
-        city: "Wisokyburgh",
-        zipcode: "90566wedfsrf-7771",
-        geo: {
-          lat: "-43.9509",
-          lng: "-34.4618",
-        },
-      },
-      phone: "010-692-6593 x09125",
-      website: "anastasia.net",
-      company: {
-        name: "Deckow-Crist",
-        catchPhrase: "Proactive didactic contingency",
-        bs: "synergize scalable supply-chains",
-      },
-    },
   ]);
 
   console.log(`New request getdata version:${version}` + req.url);
