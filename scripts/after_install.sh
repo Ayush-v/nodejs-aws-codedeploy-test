f289b300fa600f091aad8b4d8fd4ed39609f8edd
Ayush <62958067+Ayush-v@users.noreply.github.com>
2022-11-19 15:04:12 +0530

    testing

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,9 +3,6 @@
 #give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ec2-user/nodejs-aws-codedeploy-test
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
 echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
 
 echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-
 echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
 
-
-cd /home/ec2-user/nodejs-aws-codedeploy-test
-echo 'sudo pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
+echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
 pm2 restart nodejs-express-app >> /home/ec2-user/nodejs-aws-codedeploy-test/deploy.log
